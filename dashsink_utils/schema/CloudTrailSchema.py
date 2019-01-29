08a47c994e3c178394f719a54704b05bf3da1aae
nslam <nslamgg@gmail.com>
2019-01-29 23:33:54 +0800

    update cloudtrail schema

--- dashsink_utils/schema/CloudTrailSchema.py
+++ dashsink_utils/schema/CloudTrailSchema.py
@@ -1,2 +1,18 @@
-cloudTrailSchema={
+cloudTrailSchema = {
+    "eventVersion": "sorted",
+    "userIdentity": {
+        "type": "sorted",
+        "principalId": "sorted",
+        "arn": "sorted",
+        "accountId": "sorted",
+        "accessKeyId": "sorted",
+        "userName": "sorted"
+    },
+    "eventSource": "sorted",
+    "eventName": "sorted",
+    "awsRegion": "sorted",
+    "sourceIPAddress": "sorted",
+    "userAgent": "sorted",
+    "requestParameters": "text",
+    "responseElements": "text"
 }
