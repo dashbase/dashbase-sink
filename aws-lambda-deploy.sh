8e9601bd6ac42cccc7a9309a956e38fcec3d5e27
nslam <nslamgg@gmail.com>
2019-01-29 20:50:29 +0800

    update lambda deploy script

--- aws-lambda-deploy.sh
+++ aws-lambda-deploy.sh
@@ -5,4 +5,5 @@ cd env/lib/python3.7/site-packages/ && \
 zip -r9 ../../../../s3ToKafka.zip * && \
 cd ../../../../ && \
 zip -r9 ./s3ToKafka.zip dashsink_utils/ && \
-zip -g ./s3ToKafka.zip ./s3ToKafka.py
+zip -g ./s3ToKafka.zip ./s3ToKafka.py && \
+aws lambda update-function-code --function-name cloudtrailToDashbase --zip-file fileb://s3ToKafka.zip --region us-west-2
