c46e5a1b879a6c5374d2a219ef752905cb3390b5
nslam <nslamgg@gmail.com>
2019-02-23 17:43:05 +0800

    add function without kafka

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dashsink_utils.MessagePackBuilder import MessagePackDocBuilder
 from dashsink_utils.schema.GoogleCloudLogEntrySchema import logEntrySchema
 import ujson, zulu
 import os
+import requests
 
 kafka_host = os.environ.get('KAFKA_HOST', '35.247.63.148:9092')
 topic = os.environ.get('KAFKA_TOPIC', 'gcloud-sink')
@@ -12,7 +13,7 @@ schema = logEntrySchema
 
 
 # TODO parse gcloud log entry
-def dash_sink(event, context):
+def dash_sink_with_kafka(event, context):
     """Triggered by a change to a Cloud Storage bucket.
     Args:
          event (dict): Event payload.
@@ -41,6 +42,33 @@ def dash_sink(event, context):
         produce_data(producer, topic, builder.build(), key='key')
     producer.flush()
 
+
+def dash_sink(event, context):
+    file = event
+    print(f"Processing file: {file['name']}.")
+    data = get_blob_data(bucket_name='dashbase-stackdriver-logging', source_blob_name=file['name']).decode().strip()
+    logs = data.split('\n')
+    index = '{"index": { "_index": "gcloud-sink", "_type": "sink"}}\n'
+    bulk_request=''
+    for i in range(0,len(logs),1):
+        bulk_request +=index+logs[i]+'\n'
+        if (i+1) % 100==0:
+            post_bulk(bulk_request.strip())
+            bulk_request=''
+    post_bulk(bulk_request.strip())
+    bulk_request = ''
+    print("All the steps finished")
+
+def post_bulk(data):
+    if data=='':
+        return
+    url = 'https://35.247.115.177:31514/_bulk'
+    headers = {
+        'content-type': 'application/json',
+        'cache-control': 'no-cache'
+    }
+    r = requests.post(url, data=data, headers=headers, verify=False)
+    print(f"Bulk result:", r.status_code)
 
 def flatten(builder, prefix, value, dashbase_type):
     if isinstance(value, dict):
@@ -94,21 +122,7 @@ def produce_data(producer, topic, data, key=None):
 
 
 def main():
-    builder = MessagePackDocBuilder()
-    builder.set_timestamp(1234)
-    builder.set_raw('This is test')
-    builder.put_text('text', 'abc')
-    builder.put_text_offset('text2', 0, 4)
-    builder.put_sorted("sorted", "abc")
-    builder.put_sorted_offset("sorted2", 8, 12)
-    builder.put_int('long', 123)
-    builder.put_double('double', 12.34)
-    builder.put_lat_lon('latlon', 12, 34)
-    k = builder.build()
-    dash_sink({'file': '1'}, 1)
-    # global kafka_server
-    # kafka_server = 'localhost'
-    # dash_sink({'name': 'web/2018/12/30/00:00:00_00:59:59_S0.json'}, None)
+    dash_sink(1,1)
 
 
 if __name__ == '__main__':
